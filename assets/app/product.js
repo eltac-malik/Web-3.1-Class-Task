adba2bbf8225576c336c65f9d5f65f6911fe163f
eltac-malik <eltacem@code.edu.az>
2023-02-07 22:44:06 +0400

    develop

---
+++ assets/app/product.js
@@ -0,0 +1,458 @@
+let products = [
+    {
+        id: 1,
+        title: "Lay's Mevsim Yeşillikli",
+        image: "https://cdn.getir.com/product/5bc97aabb137fb001d751ac7_tr_1609123518121.jpeg",
+        alt: "96 g",
+        price: 6.36,
+        category: "qida",
+        count:1
+    },
+    {
+        id: 2,
+        title: "Eti Canga",
+        image: "https://cdn.getir.com/product/5f3538dc71a71226677fba17_tr_1624437105104.jpeg",
+        alt: "45 g",
+        price: 2.85,
+        category: "qida",
+        count : 1
+    },
+    {
+        id: 3,
+        title: "Kuzeyden",
+        image: "https://cdn.getir.com/product/5a7b20fd8e19da0004bb27f8_tr_1615375864268.jpeg",
+        alt: "1.5 L",
+        price: 2.95,
+        category: "qida",
+        count : 1
+    },
+    {
+        id: 4,
+        title: "Kızılay Erzincan",
+        image: "https://cdn.getir.com/product/60018c5bca2126d84459c47f_tr_1610788125046.jpeg",
+        alt: "6 x 200 ml",
+        price: 5.9,
+        category: "qida",
+        count : 1
+    },
+    {
+        id: 5,
+        title: "Ülker Napoliten",
+        image: "https://cdn.getir.com/product/574b2219dee8a90300f18d24_tr_1599938278519.jpeg",
+        alt: "33 g",
+        price: 3.3,
+        category: "qida",
+        count : 1
+    },
+    {
+        id: 6,
+        title: "Kavrulmuş Siyah Ay Çekirdeği",
+        image: "https://cdn.getir.com/product/5ccaf4ff1a1763000175a869_tr_1580904995656.jpeg",
+        alt: "180 g",
+        price: 10.95,
+        category: "qida",
+        count : 1
+    },
+    {
+        id: 7,
+        title: "Magnum Badem",
+        image: "https://cdn.getir.com/product/559fec01f462100c00461d5c_tr_1618323765236.jpeg",
+        alt: "100 ml",
+        price: 7.5,
+        category: "qida",
+        count : 1
+    },
+    {
+        id: 8,
+        title: "İçim %3 Yağlı Süt",
+        image: "https://cdn.getir.com/product/5ced482d4a8a2a000137da6d_tr_1623652387464.jpeg",
+        alt: "1 L",
+        price: 7.95,
+        category: "qida",
+        count : 1
+    },
+    {
+        id: 9,
+        title: "Lay's Mevsim Yeşillikli",
+        image: "https://cdn.getir.com/product/5bc97aabb137fb001d751ac7_tr_1609123518121.jpeg",
+        alt: "96 g",
+        price: 6.36,
+        category: "qida",
+        count : 1
+    },
+    {
+        id: 10,
+        title: "Eti Canga",
+        image: "https://cdn.getir.com/product/5f3538dc71a71226677fba17_tr_1624437105104.jpeg",
+        alt: "45 g",
+        price: 2.85,
+        category: "qida",
+        count : 1
+    },
+    {
+        id: 11,
+        title: "Kuzeyden",
+        image: "https://cdn.getir.com/product/5a7b20fd8e19da0004bb27f8_tr_1615375864268.jpeg",
+        alt: "1.5 L",
+        price: 2.95,
+        category: "qida",
+        count : 1
+    },
+    {
+        id: 12,
+        title: "Kızılay Erzincan",
+        image: "https://cdn.getir.com/product/60018c5bca2126d84459c47f_tr_1610788125046.jpeg",
+        alt: "6 x 200 ml",
+        price: 5.9,
+        category: "qida",
+        count : 1
+    },
+    {
+        id: 13,
+        title: "Ülker Napoliten",
+        image: "https://cdn.getir.com/product/574b2219dee8a90300f18d24_tr_1599938278519.jpeg",
+        alt: "33 g",
+        price: 3.3,
+        category: "qida",
+        count : 1
+    },
+    {
+        id: 14,
+        title: "Kavrulmuş Siyah Ay Çekirdeği",
+        image: "https://cdn.getir.com/product/5ccaf4ff1a1763000175a869_tr_1580904995656.jpeg",
+        alt: "180 g",
+        price: 10.95,
+        category: "qida",
+        count : 1
+    },
+    {
+        id: 15,
+        title: "Magnum Badem",
+        image: "https://cdn.getir.com/product/559fec01f462100c00461d5c_tr_1618323765236.jpeg",
+        alt: "100 ml",
+        price: 7.5,
+        category: "qida",
+        count : 1
+    },
+    {
+        id: 16,
+        title: "İçim %3 Yağlı Süt",
+        image: "https://cdn.getir.com/product/5ced482d4a8a2a000137da6d_tr_1623652387464.jpeg",
+        alt: "1 L",
+        price: 7.95,
+        category: "qida",
+        count : 1
+    },
+    {
+        id: 17,
+        title: "Colgate Optik White",
+        image: "https://cdn.getir.com/product/5c3f4bdcf9ba5f0001e7b0d9_tr_1609902603858.jpeg",
+        alt: "75ml",
+        price: 39.9,
+        category: "qida",
+        count : 1
+    },
+    {
+        id: 18,
+        title: "Nuh'un Ankara Spaghetti",
+        image: "https://cdn.getir.com/product/559fb12df462100c00461494.jpeg",
+        alt: "500 g",
+        price: 7.5,
+        category: "qida",
+        count : 1
+    },
+    {
+        id: 19,
+        title: "ttec Rio Kulaklık",
+        image: "https://cdn.getir.com/product/5d396c817e10840001b69e50_tr_1633087074079.jpeg",
+        alt: "Siyah",
+        price: 79.9,
+        category: "tech",
+        count : 1
+    },
+    {
+        id: 20,
+        title: "taks Taşınabilir Hoparlör",
+        image: "https://cdn.getir.com/product/60d1bedcb2d16d680d91b92f_tr_1624875462028.jpeg",
+        alt: "Kırmızı",
+        price: 269,
+        category: "tech",
+        count : 1
+    },
+    {
+        id: 21,
+        title: "Üzüm",
+        image: "https://cdn.getir.com/product/5f2162a356b9b125791d57ba_tr_1596543638749.jpeg",
+        alt: "500 g",
+        price: 6.31,
+        category: "qida",
+        count : 1
+    },
+    {
+        id: 22,
+        title: "Hertane Kuru Sele Zeytin",
+        image: "https://cdn.getir.com/product/6012c56c18495db74f09d75c_tr_1638949311938.jpeg",
+        alt: "400 g",
+        price: 47.65,
+        category: "qida",
+        count : 1
+    },
+    {
+        id: 23,
+        title: "Balparmak Çiçek Balı",
+        image: "https://cdn.getir.com/product/5683e84c06df100f0046fc8f_tr_1609395786468.png",
+        alt: "350 g",
+        price: 50.03,
+        category: "qida",
+        count : 1
+    },
+    {
+        id: 24,
+        title: "Nutella Kakaolu Fındık Kreması",
+        image: "https://cdn.getir.com/product/5d1df4b81976b40001fdc590_tr_1602050602002.jpeg",
+        alt: "750 g",
+        price: 44.14,
+        category: "qida",
+        count : 1
+    },
+    {
+        id: 25,
+        title: "CP Piliç Lokum",
+        image: "https://cdn.getir.com/product/5ee1d6d05398f97f3bfa7a6b_tr_1601151033199.jpeg",
+        alt: "300 g",
+        price: 29.9,
+        category: "qida",
+        count : 1
+    },
+    {
+        id: 26,
+        title: "Sek %3 Yağlı Barista Sütü",
+        image: "https://cdn.getir.com/product/5d9f05637cb438b48c2e0c12_tr_1589468554107.jpeg",
+        alt: "1 LT",
+        price: 15.85,
+        category: "qida",
+        count : 1
+    },
+    {
+        id: 27,
+        title: "Taciroğlu Tulum",
+        image: "https://cdn.getir.com/product/5ced482ed349d10001e70544_tr_1630679028032.jpeg",
+        alt: "250 gr",
+        price: 33.75,
+        category: "qida",
+        count : 1
+    },
+    {
+        id: 28,
+        title: "Filiz Fiyonk",
+        image: "https://cdn.getir.com/product/5cf27249130f3a00011f4d99_tr_1586450111921.jpeg",
+        alt: "500 gr",
+        price: 7.55,
+        category: "qida",
+        count : 1
+    },
+    {
+        id: 29,
+        title: "Dardanel Zeytinyağlı Ton",
+        image: "https://cdn.getir.com/product/5e9d569eca76537bc73182b9_tr_1604781892181.jpeg",
+        alt: "150 gr",
+        price: 37.36,
+        category: "qida",
+        count : 1
+    },
+    {
+        id: 30,
+        title: "Tat Jalapeno Biber Turşusu",
+        image: "https://cdn.getir.com/product/5d03bb6789c5f10001c36b45_tr_1597516999634.jpeg",
+        alt: "330 gr",
+        price: 16.4,
+        category: "qida",
+        count : 1
+    },
+    {
+        id: 31,
+        title: "ACE Klassik",
+        image: "https://cdn.getir.com/product/6050b714cb3aea1cfeaf01a3_tr_1632835369035.jpeg",
+        alt: "4 LT",
+        price: 27.37,
+        category: "temizlik",
+        count : 1
+    },
+    {
+        id: 32,
+        title: "Vanish Halı Leke Çıkarıcı",
+        image: "https://cdn.getir.com/product/5bb713c65902b20017b0e874_tr_1622728039390.jpeg",
+        alt: "500 ml",
+        price: 43.24,
+        category: "temizlik",
+        count : 1
+    },
+    {
+        id: 33,
+        title: "Kemal Kükrer Beyaz Sirke",
+        image: "https://cdn.getir.com/product/5ced482ad349d10001e70512_tr_1598732135370.jpeg",
+        alt: "2 LT",
+        price: 19.25,
+        category: "temizlik",
+        count : 1
+    },
+    {
+        id: 34,
+        title: "Ariel Parlak Renkler",
+        image: "https://cdn.getir.com/product/5da5a2179cba107a131bf4a5_tr_1579102179864.jpeg",
+        alt: "5 LT",
+        price: 45.75,
+        category: "temizlik",
+        count : 1
+    },
+    {
+        id: 35,
+        title: "Yumoş Extra Lilyum",
+        image: "https://cdn.getir.com/product/5ac780d556352c000435251f_tr_1648111429270.jpeg",
+        alt: "1440 ml",
+        price: 36.6,
+        category: "temizlik",
+        count : 1
+    },
+    {
+        id: 36,
+        title: "Mandalina",
+        image: "https://cdn.getir.com/product/5bbb7a3646559c001e5a77b5_tr_1636582560894.jpeg",
+        alt: "1 Kq",
+        price: 17.45,
+        category: "qida",
+        count : 1
+    },
+    {
+        id: 37,
+        title: "Ülker Biskrem Kakao",
+        image: "https://cdn.getir.com/product/5b6c4158ac68fd0004af28c4_tr_1602361244816.jpeg",
+        alt: "184 qr",
+        price: 5.5,
+        category: "qida",
+        count : 1
+    },
+    {
+        id: 38,
+        title: "Krem Sıvı Sabun",
+        image: "https://cdn.getir.com/product/5e6b59d5370b07ce0c123ee1_tr_1603570571382.jpeg",
+        alt: "300 ml",
+        price: 27.37,
+        category: "temizlik",
+        count : 1
+    },
+    {
+        id: 39,
+        title: "Ultra Koruma Sıvı Sabun",
+        image: "https://cdn.getir.com/product/5e6a0ad7d21bd81074d9f507_tr_1603570600663.jpeg",
+        alt: "300 ml",
+        price: 27.37,
+        category: "temizlik",
+        count : 1
+    },
+    {
+        id: 40,
+        title: "Privacy Erkek Hediye Seti",
+        image: "https://cdn.getir.com/product/5fdccb7e8846ce38d56c92c2_tr_1608879840625.jpeg",
+        alt: "2 eded",
+        price: 149.9,
+        category: "temizlik",
+        count : 1
+    }
+]
+
+let all = document.getElementById("all-product")
+let nav = document.getElementById("nav")
+let store = document.getElementById("store")
+let sup = document.getElementById('sup')
+
+nav.addEventListener("click",()=>{
+    store.style.display === "none" ?
+        store.style.display = "flex":
+        store.style.display = "none"
+})
+
+if(JSON.parse(localStorage.getItem('basket'))===null){
+    localStorage.setItem('basket',JSON.stringify([]))
+}
+
+function writer(){
+    products.forEach(e => {
+        all.innerHTML += `
+        <div class="card" >
+        <img class="card-img-top" src=${e.image} alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">${e.title}</h5>
+                <p class="card-text">${e.price}</p>
+                <a onclick='addProduct(${e.id})' class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+        `
+    })
+}
+
+function addProduct(item) {
+    let currentProd = products.find(e=> e.id === item)
+    let arr = JSON.parse(localStorage.getItem("basket"))
+    let imran = arr.find(e=> e.id === item)
+
+    if(imran === undefined){
+        localStorage.setItem('basket', JSON.stringify([...arr, currentProd]))
+    }
+    else {
+        let newArr = arr.map(e => {
+            if (e.id === item) {
+                return { ...e, count: e.count + 1 }
+            }
+            return e
+        })
+        localStorage.setItem('basket', JSON.stringify(newArr))
+    }
+    supCount()
+    basketWriter()
+}
+
+
+
+function basketWriter(){
+    let arr = JSON.parse(localStorage.getItem("basket"))
+    store.innerHTML = ''
+    arr.forEach(e=>{
+        store.innerHTML += `
+        <p>${e.title} <span>${e.count}</span>
+        <span onclick='deleteImran(${e.id})'>SIL</span>
+        </p>`
+    })
+}
+
+function supCount(){
+    let count = JSON.parse(localStorage.getItem("basket"))
+    sup.textContent = count.length
+}
+
+
+function deleteImran(id) {
+    let arr = JSON.parse(localStorage.getItem("basket"))
+    let current = arr.find(e=> e.id === id);
+
+    if (current.count <= 1){
+        let newArr = arr.filter(e=> e.id !== id)
+        localStorage.setItem("basket",JSON.stringify(newArr))
+    }
+    else {
+        let newArr = arr.map(e=> {
+            if(e.id == id){
+                return {...e, count:e.count-1}
+            }
+            return e
+        })
+        localStorage.setItem("basket", JSON.stringify(newArr))
+    }
+
+
+    basketWriter()
+    supCount()
+}
+
+
+writer()
